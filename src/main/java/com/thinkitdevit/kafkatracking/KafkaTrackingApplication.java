e6887fe2a7f96117423d6ee1a22cd6f0bb05bebd
johndbs <83272366+johndbs@users.noreply.github.com>
2024-05-21 14:34:06 +0200

    Initial commit

---
+++ src/main/java/com/thinkitdevit/kafkatracking/KafkaTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.thinkitdevit.kafkatracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkitdevit/kafkatracking/KafkaTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkitdevit.kafkatracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
